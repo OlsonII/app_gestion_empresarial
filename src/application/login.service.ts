ed2a1820862db70ac5b76931785b5315185f0ff6
Nilson Rosado <30422101+OlsonII@users.noreply.github.com>
2020-10-30 10:47:33 -0500

    Ajuste de codigo en el servicio login
    Se agrega tolerancia a fallos y ajuste de  codigo
    
    v0.17.1
    
    Co-Authored-By: TilsonP <36851632+TilsonP@users.noreply.github.com>

--- src/application/login.service.ts
+++ src/application/login.service.ts
@@ -7,18 +7,23 @@ export class LoginService {
     constructor(private readonly _unitOfWork: IUnitOfWork) {}
 
     async execute(request: LoginRequest): Promise<LoginResponse>{
-        const user: User = await this._unitOfWork.userRepository.findUser(request.identification);
-        if(user != undefined){
-            if (user.password == request.password){
-                const token = new Token();
-                user.token = token.generateToken();
-                this._unitOfWork.start();
-                const loggedUser = await this._unitOfWork.complete(async () => await this._unitOfWork.userRepository.login(user));
-                return new LoginResponse(loggedUser);
+        try {
+            const user: User = await this._unitOfWork.userRepository.findUser(request.identification);
+            if(user != undefined){
+                if (user.password == request.password){
+                    const token = new Token();
+                    user.token = token.generateToken();
+                    this._unitOfWork.start();
+                    const loggedUser = await this._unitOfWork.complete(async () => await this._unitOfWork.userRepository.login(user));
+                    return new LoginResponse(loggedUser);
+                }
+                return new LoginResponse(undefined, 'Contraseña incorrecta');
             }
-            return new LoginResponse(undefined, 'Contraseña incorrecta');
+            return new LoginResponse(undefined, 'Este usuario no se encuentra registrado');
+        }catch (e){
+            return new LoginResponse(undefined, 'Ha habido un error');
         }
-        return new LoginResponse(undefined, 'Este usuario no se encuentra registrado');
+
     }
 }
 
