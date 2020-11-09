1fb0eabc843a1519fff329423d1a6d004ab39a3d
Rafael Camelo <rafaelgcamelo@gmail.com>
2020-11-09 17:58:35 -0500

    Movimientos

--- src/presentation/src/app/pages/movement/movement.component.ts
+++ src/presentation/src/app/pages/movement/movement.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {MovementService} from '../../services/movement.service';
-import {Movement} from '../../models/movemet.model';
+import {Movement} from '../../models/movement.model';
 
 @Component({
   selector: 'app-movement',
@@ -20,9 +20,23 @@ export class MovementComponent implements OnInit {
       if (data != null){
         console.log(data.movements);
         this.movements = data.movements;
+        this.movements.forEach( (movement) => {
+
+          if (movement.outputQuantity != '0'){
+            movement.quantity = movement.outputQuantity;
+            movement.type = 'Salida';
+          }
+
+          if (movement.inputQuantity != '0'){
+            movement.quantity = movement.inputQuantity;
+            movement.type = 'Entrada';
+          }
+        } )
         this.movements.reverse();
       }
     });
   }
+
+
 
 }
