9fed530fff97f3488fa1739635c7e09c0f8e4426
Carlos Baquero <karlosmario0123@gmail.com>
2020-11-29 14:58:00 -0500

    correccion de sale.controller

--- src/controller/sale.controller.ts
+++ src/controller/sale.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Post } from '@nestjs/common';
+import { Body } from '@nestjs/common/decorators/http/route-params.decorator';
 import { RegisterSaleRequest, RegisterSaleService } from '../application/register.sale.service';
 import { UnitOfWork } from '../infrastructure/unitOfWork/unitOfWork';
 
@@ -8,7 +9,7 @@ export class SaleController{
   constructor(private readonly _unitOfWork: UnitOfWork) {}
 
   @Post()
-  async registerSale(request: RegisterSaleRequest){
+  async registerSale(@Body() request: RegisterSaleRequest){
     const service: RegisterSaleService = new RegisterSaleService(this._unitOfWork);
     return await service.execute(request);
   }
