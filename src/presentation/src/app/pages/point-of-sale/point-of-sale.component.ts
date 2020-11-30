a5cdf825147e68bc2b14b2cf2fd2f02347b4cce1
cristian jhair mejia navarro <kristianlcj@outlook.com>
2020-11-30 00:01:43 -0500

    Registro de cliente en ventas

--- src/presentation/src/app/pages/point-of-sale/point-of-sale.component.ts
+++ src/presentation/src/app/pages/point-of-sale/point-of-sale.component.ts
@@ -1,34 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+/* tslint:disable:triple-equals */
+import { Component, OnDestroy, OnInit, TemplateRef } from '@angular/core';
 import {Product}from '../../models/product.model';
 import {ProductService}from '../../services/product.service';
 import {Sale} from '../../models/sale.model';
 import {SalesService} from '../../services/sales.service';
 import {JwtAuthService} from '../../services/auth/jwt-auth.service';
 import { Client } from '../../models/client.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ClientService } from '../../services/client.service';
+import { ModalDismissReasons, NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-point-of-sale',
   templateUrl: './point-of-sale.component.html',
   styleUrls: ['./point-of-sale.component.scss']
 })
-export class PointOfSaleComponent implements OnInit {
+export class PointOfSaleComponent implements OnInit,OnDestroy {
 
   productos:Product[] =[];
+  clients: Client[];
   totalSale = 0;
   productosFactura:Product[]=[];
   searchValue = '';
   public isCollapsed = true;
+  form: FormGroup;
+  client:Client;
+  submitted = false;
+  closeResult = '';
+  formClientValidate = false;
 
-  cliente:Client;
-
-  constructor(private productService:ProductService,
+  constructor(
+    private productService:ProductService,
     private salesService:SalesService,
-    private jwtAuth:JwtAuthService
-    ) { }
+    private jwtAuth:JwtAuthService,
+    private formBuilder: FormBuilder,
+    private clientService: ClientService,
+    private modalService: NgbModal,
+    public activeModal: NgbActiveModal,
+    private toastr:ToastrService,
+  ) { }
 
   ngOnInit(): void {
     window.addEventListener('resize', this.updateColor);
     this.getProducts();
+    this.client = new Client();
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      identification: ['', Validators.required],
+      telephone: ['', Validators.required],
+      street: ['', Validators.required],
+      email: ['', Validators.required],
+    });
+  }
+
+  get f() { return this.form.controls; }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  dataClient(){
+    this.formClientValidate = true;
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
   getProducts(){
@@ -41,7 +88,53 @@ export class PointOfSaleComponent implements OnInit {
     )
   }
 
+  showNotification(titulo, mensaje,from, align){
+    this.toastr.info('<span class="tim-icons icon-check-2" [data-notify]="icon"></span>'+mensaje, titulo, {
+      disableTimeOut: true,
+      closeButton: true,
+      enableHtml: true,
+      toastClass: 'alert alert-success alert-with-icon',
+      positionClass: 'toast-' + from + '-' +  align
+    });
 
+  }
+
+  addClient() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    const formData = this.form.value;
+    this.client.name = formData.name;
+    this.client.identification = formData.identification;
+    this.client.email = formData.email;
+    this.client.street = formData.street;
+    this.client.telephone = formData.telephone;
+    this.clientService.post(this.client).subscribe(p => {
+      if (p != null) {
+        this.formClientValidate = false;
+        this.showNotification('Registro',p.message,'bottom','right');
+      }
+    });
+  }
+
+  getClient(content){
+    console.log('press enter')
+    let result = false;
+    this.clientService.get().subscribe(res=> {
+      res.clients.forEach(client => {
+        if (client.identification === this.form.value.identification) {
+          this.client = client;
+          this.form.controls.name.setValue(client.name);
+          this.form.controls.email.setValue(client.email);
+          result = true;
+        }
+      })
+      if (result == false) {
+        this.open(content);
+      };
+    });
+  }
 
   getTotalSale(){
     let subTotal = 0;
@@ -87,7 +180,7 @@ export class PointOfSaleComponent implements OnInit {
   registrarVenta(){
     const venta = new Sale();
     venta.products = this.productosFactura;
-    venta.clientIdentification = '333'
+    venta.clientIdentification = this.form.value.identification;
     venta.value = this.totalSale;
     console.log(venta);
     this.salesService.post(venta).subscribe(res=>{
